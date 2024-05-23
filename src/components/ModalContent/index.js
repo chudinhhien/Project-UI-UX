781e57d0db560b775d4c2f76aae7879fc480bcd6
Chu Đình Hiển <hienc1212@gmail.com>
2024-05-23 20:43:38 +0700

    merge dashboard

--- src/components/ModalContent/index.js
+++ src/components/ModalContent/index.js
@@ -1,4 +1,3 @@
-// ModalContent.js
 import React from 'react';
 import { Form, Input, Upload, Button, Row, Col, Table } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
@@ -17,22 +16,22 @@ const columns = [
   },
 ]
 
-const ModalContent = ({ form, handleAddTarget,targets }) => {
+const ModalContent = ({ form, handleAddTarget, targets }) => {
   return (
     <Form layout='vertical' form={form}>
       <h1>Thông tin chung:</h1>
-      <Row gutter={20}>
-        <Col sm={12}>
+      <Row gutter={[20, 10]}>
+        <Col xs={24} sm={12}>
           <Form.Item name="name" label="Name:">
             <Input />
           </Form.Item>
         </Col>
-        <Col sm={12}>
+        <Col xs={24} sm={12}>
           <Form.Item name="thumbnail" label="Link avatar:">
             <Input />
           </Form.Item>
         </Col>
-        <Col sm={24}>
+        <Col xs={24}>
           <Form.Item name="description" label="Mô tả:">
             <TextArea />
           </Form.Item>
@@ -40,31 +39,30 @@ const ModalContent = ({ form, handleAddTarget,targets }) => {
       </Row>
 
       <h1>Danh sách mục tiêu:</h1>
-      <Row gutter={20}>
-        <Col sm={12}>
+      <Row gutter={[20, 10]}>
+        <Col xs={24} sm={12}>
           <Form.Item name="nameTarget" label="Tên mục tiêu:">
             <Input />
           </Form.Item>
         </Col>
-        <Col sm={12}>
+        <Col xs={24} sm={12}>
           <Form.Item name="unit" label="Đơn vị:">
             <Input />
           </Form.Item>
         </Col>
-        <Col sm={8}>
+        <Col xs={24} sm={8}>
           <Upload>
             <Button icon={<UploadOutlined />}>Import file</Button>
           </Upload>
         </Col>
       </Row>
-      <Row justify="end" gutter={20}>
+      <Row justify="end" gutter={[20, 20]}>
         <Col>
-          <Button type='primary' onClick={handleAddTarget} style={{marginBottom: '20px'}}>Add Target</Button>
+          <Button type='primary' onClick={handleAddTarget} style={{ marginBottom: '20px' }}>Add Target</Button>
         </Col>
       </Row>
-      <Table columns={columns} dataSource={targets}/>
+      <Table columns={columns} dataSource={targets} />
     </Form>
-    
   );
 };
 
