5ab9422033f4eb4993699fb3ba1c2d1b9e9c1158
Chu Đình Hiển <hienc1212@gmail.com>
2024-05-24 13:01:47 +0700

    update setting

--- src/pages/Setting/index.js
+++ src/pages/Setting/index.js
@@ -1,7 +1,28 @@
-function Setting(){
-  return(
+import { Tabs } from "antd";
+import './Setting.scss'
+
+const items = [
+  {
+    key: '1',
+    label: 'Edit Profile',
+    children: 'Content of Tab Pane 1',
+  },
+  {
+    key: '2',
+    label: 'Security',
+    children: 'Content of Tab Pane 2',
+  }
+]
+
+
+function Setting() {
+
+  return (
     <>
-      <h1>Setting</h1>
+      <div className="custom-container">
+        <Tabs defaultActiveKey="1" items={items} style={{ backgroundColor: '#ffffff', marginTop: '30px', borderRadius: '25px', padding: '30px' }}/>
+      </div>
+
     </>
   )
 }
